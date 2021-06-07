fc5af6edac5f132f8faafda708515872b5695116
Steve Rydz <steverydz@gmail.com>
2021-06-07 22:01:16 +0100

    Update new post script

--- new-post.sh
+++ new-post.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 
 POST_TITLE=$1
-POST_SLUG=$(echo "$POST_TITLE" | tr " " "-" | tr "[:upper:]" "[:lower:]")
+POST_SLUG=$(echo "$POST_TITLE" | tr " " "-" | tr -d "'" | tr "[:upper:]" "[:lower:]")
 POSTS_PATH="src/posts/$POST_SLUG.md"
 
 touch $POSTS_PATH
 
 echo "---" >> $POSTS_PATH
 echo "date: Created" >> $POSTS_PATH
-echo "layout: default" >> $POSTS_PATH
+echo "layout: post" >> $POSTS_PATH
 echo "tags: post" >> $POSTS_PATH
 echo "permalink: \"/{{ page.date | date: '%Y/%m/%d' }}/$POST_SLUG/index.html\"" >> $POSTS_PATH
 echo "title": $POST_TITLE >> $POSTS_PATH
